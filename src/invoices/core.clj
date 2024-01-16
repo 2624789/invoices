87c24bc2e8cf1190913ebeb4dc0d42b52ac85c60
2624789 <2624789@users.noreply.github.com>
2024-01-16 11:58:45 -0500

    Converts JSON file to invoice map

--- src/invoices/core.clj
+++ src/invoices/core.clj
@@ -1,12 +1,20 @@
 (ns invoices.core
-  (:require [invoice-items :as ii])
+  (:require [invoice-items :as ii]
+            [clojure.data.json :as json])
   (:use clojure.pprint)
   (:gen-class))
 
 (def invoice (clojure.edn/read-string (slurp "invoice.edn")))
 
+(defn file->invoice
+  "Converts an invoice json file to a clojure map"
+  [file-name]
+  (json/read-str (slurp file-name)))
+
 (defn -main
   "Demo features"
   [& args]
-  (do (println "Invoice items that satisfy given conditions:\n")
-      (pprint (ii/find-invoice-items (:invoice/items invoice)))))
+  (do (println "\nInvoice items that satisfy given conditions:\n")
+      (pprint (ii/find-invoice-items (:invoice/items invoice)))
+      (println "\nInvoice from JSON file:\n")
+      (pprint (file->invoice "invoice.json"))))
