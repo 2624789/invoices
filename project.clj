4039271a2d2fbb9985f9e92b70b50831ed1e6841
2624789 <2624789@users.noreply.github.com>
2024-01-16 07:13:04 -0500

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject invoices "0.1.0-SNAPSHOT"
+  :description "Clojure invoices"
+  :url "https://github.com/2624789/invoices"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot invoices.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ src/invoices/core.clj
@@ -0,0 +1,7 @@
+(ns invoices.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
